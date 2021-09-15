ea23e580472a619b0a22daf11693c1dc2f7cbf1b
frankjlin16 <flin30@asu.edu>
2021-09-14 22:18:17 -0700

    Added grids

--- src/components/gridBox.js
+++ src/components/gridBox.js
@@ -1,15 +1,70 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const GridBox = () => {
-    return (
-        <div className="relative bg-white overflow-hidden">
-            <div className="table w-full">
-                <div className="table-row">
-                    <div className="table-cell">Cell 1</div>
-                </div>
-            </div>
-        </div>
-    )
-}
+  const [gridMatrix, setGridMatrix] = useState([
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+    { id: 7 },
+    { id: 8 },
+    { id: 9 },
+    { id: 10 },
+    { id: 11 },
+    { id: 12 },
+    { id: 13 },
+    { id: 14 },
+    { id: 15 },
+    { id: 16 },
+    { id: 17 },
+    { id: 18 },
+    { id: 19 },
+    { id: 20 },
+    { id: 21 },
+    { id: 22 },
+    { id: 23 },
+    { id: 24 },
+    { id: 25 },
+    { id: 26 },
+    { id: 27 },
+    { id: 28 },
+    { id: 29 },
+    { id: 30 },
+    { id: 31 },
+    { id: 32 },
+    { id: 33 },
+    { id: 34 },
+    { id: 35 },
+    { id: 36 },
+    { id: 37 },
+    { id: 38 },
+    { id: 39 },
+    { id: 40 },
+    { id: 41 },
+    { id: 42 },
+    { id: 43 },
+    { id: 44 },
+    { id: 45 },
+    { id: 46 },
+    { id: 47 },
+    { id: 48 },
+  ]);
 
-export default GridBox
+	console.log(gridMatrix[1].id);
+
+  return (
+    <div className="p-4">
+      <div className="grid gap-4 grid-cols-12">
+        {gridMatrix.map((grid) => (<div key={grid.id} className="bg-purple-400 bg-opacity-50 rounded-md h-12">
+            <p className="text-center">{grid.id}</p>
+          </div>)
+          
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default GridBox;
