8e8fa2181bdc930bb1cf743dba3480f94bbadc26
frankjlin16 <flin30@asu.edu>
2021-09-14 19:11:57 -0700

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar'
 
 function App() {
   return (
     <div className="App">
-    <h1>Navbar Here</h1>
+    <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
