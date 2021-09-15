94649064a2f9645db35e5e06f03517edd5f32d6c
frankjlin16 <flin30@asu.edu>
2021-09-14 20:50:54 -0700

    Implementing grid box

---
+++ src/components/gridBox.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const GridBox = () => {
+    return (
+        <div className="relative bg-white overflow-hidden">
+            <div className="table w-full">
+                <div className="table-row">
+                    <div className="table-cell">Cell 1</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default GridBox
--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import Hero from "./hero";
+import GridBox from "./gridBox"
 
 const Home = () => {
   return (
     <React.Fragment>
       <Hero />
+      <GridBox />
     </React.Fragment>
   );
 };
