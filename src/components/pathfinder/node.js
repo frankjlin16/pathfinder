2504de7d7994417d1b7464de5811beee483ad13d
frankjlin16 <flin30@asu.edu>
2021-09-26 23:24:57 -0700

    Format code

--- src/components/pathfinder/node.js
+++ src/components/pathfinder/node.js
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
 
 const Node = (props) => {
-  var {isWall, row, column} = props;
+  var { isWall, row, column } = props;
   var className = "bg-opacity-50 h-7 w-7 m-0.5";
 
   //Chanage grid class property based on grid type
   if (!isWall) {
-    className += " bg-purple-400"
+    className += " bg-purple-400";
   } else {
-    className += " bg-green-400"
+    className += " bg-green-400";
   }
 
   return (
-    <div id={`node-${row}-${column}`} onClick={() => props.onclick(row, column)} className={className}>
-    </div>
+    <div
+      id={`node-${row}-${column}`}
+      onClick={() => props.onclick(row, column)}
+      className={className}
+    ></div>
   );
 };
 
