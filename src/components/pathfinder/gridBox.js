55891b29b5f789ea0a54495c5d8a222a68d3749c
frankjlin16 <flin30@asu.edu>
2021-09-26 23:23:22 -0700

    Updated grid UI

--- src/components/pathfinder/gridBox.js
+++ src/components/pathfinder/gridBox.js
@@ -16,9 +16,9 @@ const GridBox = () => {
 
   return (
     <div className="px-5 py-7 bg-gray-800">
-      <div className="grid p-4 bg-white rounded-md">
+      <div className="flex flex-col p-4 bg-white rounded-md">
         {grid.map((row, rowIndex) => (
-          <div key={rowIndex}>
+          <div key={rowIndex} className="flex flex-row">
             {row.map((node, nodeIndex) => (
               <Node
                 key={nodeIndex}
--- src/components/pathfinder/node.js
+++ src/components/pathfinder/node.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 
-const Grid = (props) => {
-  var { key, isWall, row, column} = props;
-  var className = "bg-opacity-50 h-7 w-7";
+const Node = (props) => {
+  var {isWall, row, column} = props;
+  var className = "bg-opacity-50 h-7 w-7 m-0.5";
 
   //Chanage grid class property based on grid type
   if (!isWall) {
@@ -12,9 +12,9 @@ const Grid = (props) => {
   }
 
   return (
-    <div id={key} onClick={() => props.onclick(row, column)} className={className}>
+    <div id={`node-${row}-${column}`} onClick={() => props.onclick(row, column)} className={className}>
     </div>
   );
 };
 
-export default Grid;
+export default Node;
