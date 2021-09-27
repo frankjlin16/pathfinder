1d3fee947e27ca0888e97991b7cdc669c17d86e3
frankjlin16 <flin30@asu.edu>
2021-09-26 22:21:57 -0700

    Updated create grid and create node function

--- src/components/pathfinder/gridBox.js
+++ src/components/pathfinder/gridBox.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Grid from "./node";
 
+const rows = 15;
+
 const GridBox = () => {
   const [gridMatrix, setGridMatrix] = useState(createGrid());
 
@@ -26,13 +28,31 @@ const GridBox = () => {
   );
 };
 
-// Creates a grid matrix with 48 individual grids
+// Creates a grid matrix with 15 rows and 20 cols
 const createGrid = () => {
-  var grids = [];
-  for (let i = 0; i < 48; i++) {
-    grids.push({ id: i, type: false });
+  var grid = [];
+  for (let row = 0; row < rows; row++) {
+    const currentRow= [];
+    for (let column = 0; column < 20; column++) {
+      currentRow.push(createNode(row, column));
+    }
+    grid.push(currentRow);
   }
-  return grids;
+  return grid;
 };
+
+// Create individual nodes
+const createNode = (row, column) => {
+  return {
+    row, 
+    column, 
+    isStart: null,
+    isFinish: null,
+    isWall: false,
+    isVisited: false,
+    distance: Infinity,
+    previousNode: null,
+  }
+}
 
 export default GridBox;
